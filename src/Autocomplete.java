3e5c8f63a910b7a2bdf03ed3f99a52772edaab36
Hebert Carbonel <hebert.carbonel@gmail.com>
2016-12-02 00:15:39 -0600

    Panel  I -Auto suggester function -  help() -Closed Conection for Help() - LAST ENTRY LABEL ADDED

--- src/Autocomplete.java
+++ src/Autocomplete.java
@@ -1,5 +1,6 @@
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import java.util.List;
@@ -12,14 +13,16 @@ import java.util.logging.Logger;
 public class Autocomplete {
 
     public List autocomplete() {
-        List<String> product_list = new ArrayList<String>();
+        List<String> product_list = new ArrayList<>();
 
         ResultSet resultSet;
 
         JdbcHelper jdbcHelper = new JdbcHelper();
 
+        Statement statement = null ;
+
         try {
-            java.sql.Statement statement = jdbcHelper.conn.createStatement();
+           statement = jdbcHelper.conn.createStatement();
 
             resultSet = statement.executeQuery("SELECT product FROM tbl_expenses");
 
@@ -32,6 +35,25 @@ public class Autocomplete {
             System.out.println(product_list.size());
         } catch (SQLException ex)
         { Logger.getLogger(Expense_Service.class.getName()).log(Level.SEVERE, null, ex);
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //Closing the connection
+            try {
+                if (statement != null)
+                    statement.close();
+                }catch (SQLException ex) {
+                Logger.getLogger(Expense_Service.class.getName()).log(Level.SEVERE, null, ex);
+                try {
+                    if (jdbcHelper.conn != null)
+                    jdbcHelper.conn.close();
+            }catch (SQLException se){
+                se.printStackTrace();
+            }
+        }
+
+
         }
         return product_list;
 
